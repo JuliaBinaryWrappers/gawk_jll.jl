9c7d278c1eea41f54201502b61b9cd28db2458b9
jlbuild <juliabuildbot@gmail.com>
2025-12-05 17:24:41 +0000

    gawk_jll build 5.3.2+0

--- src/gawk_jll.jl
+++ src/gawk_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("gawk")
-JLLWrappers.@generate_main_file("gawk", UUID("054acbdd-7fbc-53f6-b51a-0fe4f8189fb8"))
+JLLWrappers.@generate_main_file("gawk", Base.UUID("054acbdd-7fbc-53f6-b51a-0fe4f8189fb8"))
 end  # module gawk_jll
